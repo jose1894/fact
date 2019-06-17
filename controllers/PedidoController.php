49ccc9880ed5ce18a942b23ab05c28d42bae262c
Jose Guerra <jose1894@gmail.com>
2019-06-17 17:18:57 -0500

    Pedidos

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -291,35 +291,9 @@ class PedidoController extends Controller
       //$modelDetalle = $modelPedido->detalles;
 
 
-
-      $content = $this->renderPartial('pedidoRpt',[
+      return $this->renderPartial('pedidoRpt',[
         'pedido'=> $modelPedido
       ]);
-      $pdf = new Pdf([
-        // set to use core fonts only
-               'mode' => Pdf::MODE_CORE,
-               // A4 paper format
-               'format' => Pdf::FORMAT_A4,
-               // portrait orientation
-               'orientation' => Pdf::ORIENT_PORTRAIT,
-               // stream to browser inline
-               'destination' => Pdf::DEST_BROWSER,
-               // your html content input
-               'content' => $content,
-               // format content from your own css file if needed or use the
-               // enhanced bootstrap css built by Krajee for mPDF formatting
-               'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/kv-mpdf-bootstrap.min.css',
-               // any css to be embedded if required
-               'cssInline' => '.kv-heading-1{font-size:18px}',
-                // set mPDF properties on the fly
-               'options' => ['title' => 'Krajee Report Title'],
-                // call mPDF methods on the fly
-               'methods' => [
-                   'SetHeader'=>['Krajee Report Header'],
-                   'SetFooter'=>['{PAGENO}'],
-                 ]
-      ]);
-      return $pdf->render();
     }
 
 
--- views/yiisoft/yii2-app/pedido/pedidoRpt.php
+++ views/yiisoft/yii2-app/pedido/pedidoRpt.php
@@ -1,7 +1,25 @@
-<h1>Pedido</h1>
 <?php
+use kartik\mpdf\Pdf;
+$pdf = Yii::$app->pdf; // or new Pdf();
+$pdf->cssFile = '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css';
+$mpdf = $pdf->api; // fetches mpdf api
+
+$mpdf->SetHTMLHeader('
+<div class="row">
+  <div class="col-lg-4">
+    <img src="img/MARVIG.JPG" height="124" width="154">
+  </div>
+  <div class="col-lg-4">
+
+  </div>
+  <div class="col-lg-4">
+
+  </div>
+</div>');
 foreach ($pedido->detalles as $key => $value) {
   // code...
   $producto = $value->productoPdetalle;
   echo $producto->cod_prod." ".$producto->des_prod."<br>";
 }
+
+echo $mpdf->Output('filename.pdf', 'I'); // call the mpdf api output as needed
