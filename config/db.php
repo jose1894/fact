4a694d86ab15edb3b598eb16326fb59afa923cc8
Jose Guerra <jose1894@gmail.com>
2020-01-27 11:32:41 -0500

    Configuracion de BBDD

--- config/db.php
+++ config/db.php
@@ -1,15 +1,31 @@
 <?php
 
-return [
+$production = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=leophard_fact',
     'username' => 'leophard',
     'password' => 'bJigtxXxFYN2S6lw',
     'charset' => 'utf8',
-  //  'tablePrefix' => 'pre_'
+    //  'tablePrefix' => 'pre_'
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    //Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
+
+$development = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=fact',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    //  'tablePrefix' => 'pre_'
+
+    //Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
+]
+
+return $production;
