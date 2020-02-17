9028b5934beff00c50e167ec4bc774b75ea4b754
Jose Guerra <jose1894@gmail.com>
2020-02-17 17:11:26 -0500

    Nota de credito 17/02/2020

--- views/yiisoft/yii2-app/nota-credito/_form.php
+++ views/yiisoft/yii2-app/nota-credito/_form.php
@@ -163,7 +163,7 @@ use app\base\Model;
               </div>
 
               <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
                   <!-- Moneda -->
                   <label class="control-label" for="nota_credito-nombre_cliente"><?= Yii::t('moneda','Currency')?></label>
                   <?= Html::input('text', 'NotaCredito[moneda_pedido]', '', [
@@ -196,21 +196,49 @@ use app\base\Model;
                                                             ]
                                                             ) ?>
                 </div>
-              </div>
-
-
-              <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                  <label class="control-label" for="nota_credito-stock_prod"><?= Yii::t('tipo_movimiento',"Movement type")?></label>
+                  <?= Html::dropDownList('NotaCredito[almacen_doc]', null, [
+                                                                              null  => 'No mover stock',
+                                                                              1     => 'Reponer stock'
+                                                                           ],
+                                                                           [
+                                                                               [ 'prompt' => Yii::t('app','Select'),],
+                                                                               'class' => 'form-control',
+                                                                               'id'    => 'nota_credito-stock_prod',
+                                                                           ]) ?>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
                   <!-- Almacen -->
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                  <!-- Condicion de pago -->
-                </div>
+                  <?php
+                    $almacenes = Almacen::getAlmacenList();
+                  ?>
+                      <label class="control-label"><?= Yii::t('almacen',"Warehouse")?></label>
+                      <?= Html::dropDownList('NotaCredito[almacen_doc]', null, $almacenes, [
+                        'class'  => 'form-control',
+                        'id'     => 'nota_credito-almacen_doc',
+                        'prompt' => Yii::t('app','Select'),
+                      ]) ?>
               </div>
-              <?php /*
+              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <!-- Condicion de pago -->
+                <?php
+                  $condp = CondPago::getCondPagoList();
+                ?>
+                    <label class="control-label"><?= Yii::t('condicionp',"Payment condition")?></label>
+                    <?= Html::dropDownList('NotaCredito[condpago_doc]', null, $condp, [
+                      'class'  => 'form-control',
+                      'id'     => 'nota_credito-condpago_doc',
+                      'prompt' => Yii::t('app','Select'),
+                    ]) ?>
+              </div>
+            </div>
+            <br>
+
               <!-- Articulos -->
               <div class="row">
                 <div class="container-fluid">
+                  <?php /*
                   <?php DynamicFormWidget::begin([
                         'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
                         'widgetBody' => '.table-body', // required: css class selector
@@ -231,7 +259,7 @@ use app\base\Model;
                             'status_pdetalle'
                         ],
                     ]); ?>
-
+                    */?>
                     <div class="row">
                         <div class="col-sm-5 col-xs-12"><?= Yii::t( 'pedido', 'Product')?></div>
                         <div class="col-sm-1 col-xs-12"><?= Yii::t( 'pedido', 'Qtty')?></div>
@@ -240,13 +268,11 @@ use app\base\Model;
                         <div class="col-sm-1 col-xs-12"><?= Yii::t( 'pedido', 'Disc.')?></div>
                         <div class="col-sm-1 col-xs-12"><?= Yii::t( 'pedido', 'Price')?></div>
                         <div class="col-sm-2 col-xs-12"><?= Yii::t( 'pedido', 'Total')?></div>
-                        <div class="col-sm-1 col-xs-12">
-                          <button type="button" class="add-item btn btn-success btn-flat btn-md" style="width:100%"  data-toggle="tooltip" data-placement="top" title="<?= Yii::t('app','Add item')?>"><i class="fa fa-plus"></i></button>
-                        </div>
                     </div>
                     <hr>
                     <div class="table-body"><!-- widgetContainer -->
-                    <?php foreach ($modelsDetalles as $index => $modelDetalle): ?>
+
+                    <?php /*foreach ($modelsDetalles as $index => $modelDetalle): ?>
                             <div class="row detalle-item"><!-- widgetBody -->
                               <div class="col-sm-5 col-xs-12">
                               <?php
@@ -369,9 +395,9 @@ use app\base\Model;
                                 <button type="button" class="remove-item btn btn-danger btn-flat btn-sm" style="width:100%" data-toggle="tooltip" data-placement="top" title="<?= Yii::t('app','Delete item')?>"><i class="fa fa-trash"></i></button>
                             </div>
                           </div>
-                    <?php endforeach; ?>
+                    <?php endforeach; */ ?>
                   </div>
-                  <?php DynamicFormWidget::end(); ?>
+                  <?php //DynamicFormWidget::end(); ?>
                   <hr>
                   <table class="table table-fixed table-stripped">
                     <tr>
@@ -400,7 +426,7 @@ use app\base\Model;
                     </tr>
                     <tr>
                       <td class="col-xs-7" style="text-align:right;">
-                        <?= Yii::t('app', 'Tax')?> <?= $IMPUESTO ?>%
+                        <?= Yii::t('app', 'Tax')?> <?php // $IMPUESTO ?>%
                       </td>
                       <td class="col-xs-2">
                         <input type="text" name="impuesto" id="impuesto" readonly class="form-control totales" value="">
@@ -446,8 +472,131 @@ use app\base\Model;
   </div>
 </div>
 
-<?php
+<div class="row detalle-item"><!-- widgetBody -->
+  <div class="col-sm-5 col-xs-12">
+  <?php
+/*
+  $resultsJs = '
+              function (data, params) {
+                params.page = params.page || 1;
+                return {
+                    results: data.results,
+                    pagination: {
+                        more: (params.page * 30) < data.results.length
+                    }
+                };
+              }';
 
+    // necessary for update action.
+    if (!$modelDetalle->isNewRecord) {
+        echo Html::activeHiddenInput($modelDetalle, "[{$index}]id_pdetalle");
+        //$modelSucursal->empresa_suc[$index] = $model->id_empresa;
+        echo Html::activeHiddenInput($modelDetalle, "[{$index}]pedido_pdetalle");
+    }
+    $url = Url::to(['producto/producto-list']);
+
+    $producto = [];
+
+    if ( !empty($modelDetalle->prod_pdetalle) )
+    {
+      $producto = Producto::findOne($modelDetalle->prod_pdetalle);
+    }
+
+    $productos = empty($modelDetalle->prod_pdetalle) ? '' : $producto->cod_prod.' '.$producto->des_prod.' - '.$producto->umedProd->des_und;
+    echo $form->field($modelDetalle, "[{$index}]prod_pdetalle",[
+      'addClass' => 'form-control ',
+      ])->widget(Select2::classname(), [
+        'language' => Yii::$app->language,
+        'initValueText' => $productos, // set the initial display text
+        'options' => ['placeholder' => Yii::t('producto','Select a product').'...'],
+        'theme' => Select2::THEME_DEFAULT,
+        'pluginOptions' => [
+            //'allowClear' => true,
+            'minimumInputLength' => 3,
+            'language' => [
+                'errorLoading' => new JsExpression("function () { return 'Waiting for results...'; }"),
+                'inputTooShort' => new JsExpression("function() {  return '".Yii::t('app','Please input {number} or more characters', [ 'number'=> 3 ])."';}"),
+            ],
+        'ajax' => [
+            'url' => $url,
+            'dataType' => 'json',
+            'method' => 'POST',
+            'data' => new JsExpression('function(params) { return {desc:params.term,tipo_listap: $("#pedido-tipo_listap").val()}; }'),
+            'processResults' => new JsExpression($resultsJs),
+        ],
+        'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+        'templateSelection' => new JsExpression( 'function (repo) { return repo.text; }'),
+        'templateResult' => new JsExpression('function (producto) {
+            if (producto.loading) {
+                return producto.text;
+            }
+
+           var markup =
+                    "<div class=\\"row\\">" +
+                        "<div class=\\"col-sm-4\\">" +
+                            "<b style=\\"margin-left:5px\\"> " + producto.cod_prod + "</b>" +
+                        "</div>" +
+                        "<div class=\\"col-sm-4\\"><b>U.M.:</b> " + producto.des_und + "</div>" +
+                        "<div class=\\"col-sm-4\\"><b>Stock:</b> " + producto.stock_prod + "</div>" +
+                    "</div>";
+
+            if (producto.des_prod) {
+              markup += "<p>" + producto.des_prod + "</p>";
+            }
+
+            return "<div style=\\"overflow:hidden;\\">" + markup + "</div>";
+
+        }'),
+        ],
+    ])->label(false);
+    ?>
+  <!-- <div class="row detalle-item"><!- widgetBody -->
+    <div class="col-sm-5 col-xs-12">
+      <input id="NotaCredito-index-prod_ddetalle" name="NotaCredito[index]prod_ddetalle" class="form-control">
+    </div>
+    <div class="col-sm-1 col-xs-12">
+      <!-- <?= $form
+      ->field($modelDetalle,"[{$index}]cant_pdetalle",[ 'addClass' => 'form-control number-integer'])
+      ->textInput(['type' => 'text'])
+      ->label(false)?> -->
+    </div>
+    <div class="col-sm-1 col-xs-12">
+      <!-- <?= $form
+      ->field($modelDetalle,"[{$index}]plista_pdetalle", [ 'addClass' => 'form-control number-decimals'])
+      ->textInput([ 'type' => 'text','readonly' => true])
+      ->label(false)?> -->
+    </div>
+    <!--div class="col-xs-1 col-xs-12">
+      <?= $form->field($modelDetalle,"[{$index}]impuesto_pdetalle")
+      ->textInput([
+        'class' => 'form-control ',
+        'style' => ['text-align' => 'right'],
+        'readonly' => true,
+      ])->label(false)?>
+    </div-->
+    <div class="col-sm-1 col-xs-12">
+      <!-- <?= $form
+      ->field($modelDetalle,"[{$index}]descu_pdetalle", [ 'addClass' => 'form-control number-decimals'])
+      ->textInput([ 'type'=>'text','width' => '200px'])
+      ->label(false)?> -->
+    </div>
+    <div class="col-sm-1 col-xs-12">
+      <!-- <?= $form
+      ->field($modelDetalle,"[{$index}]precio_pdetalle",[ 'addClass' => 'form-control number-decimals'])
+      ->textInput(['type'=>'text','width' => '200px'])
+      ->label(false)?> -->
+    </div>
+    <div class="col-sm-2 col-xs-12">
+      <!-- <?= $form
+      ->field($modelDetalle,"[{$index}]total_pdetalle",[ 'addClass' => 'form-control number-decimals'])
+      ->textInput(['type'=>'text','readonly' => true])
+      ->label(false)?> -->
+    </div>
+
+</div> -->
+
+<?php
+*/
 $js = '
 
    $( "#cod_doc" ).on( "blur", function() {
@@ -513,6 +662,17 @@ $js = '
       for(opc in data) {
         $( "input[id$=\'" + opc + "\']" ).val( data[opc] );
         $( "textarea[id$=\'" + opc + "\']" ).val( data[opc] );
+
+        if (opc == "detalle_pedido") {
+          for(deta in data[opc]) {
+            let linea  = "<div class=\\"row detalle-item\\"><!- widgetBody -->" +
+                           "<div class=\\"col-sm-5 col-xs-12\\">"+
+                                "<input id=\\"NotaCredito-" + deta + "-prod_ddetalle\\" name=\\"NotaCredito[" + deta + "]prod_ddetalle\\" class=\\"form-control\\">"+
+                            "</div>"+
+                         "</div>";
+            $( ".table-body" ).append( linea );
+          }
+        }
       }
 
    }
