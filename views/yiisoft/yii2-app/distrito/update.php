2d09e77e7f7c95874ef7fd78cb631485ae50b4b0
MariaMB17 <marialva1705@gmail.com>
2020-09-13 12:19:44 -0430

    se modifica view / update del modulo distrito tittle

--- views/yiisoft/yii2-app/distrito/update.php
+++ views/yiisoft/yii2-app/distrito/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Distrito */
 
-$this->title = Yii::t('distrito', 'Update district / parish: {<span class="label label-primary">{number}</span> {name}', [
+$this->title = Yii::t('distrito', 'Update district / parish: {{number}  / {name}', [
     'number' => $model->id_dtto,
     'name' => $model->des_dtto
 ]);
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = Yii::t('distrito', 'Update');
   <div class="box box-success">
       <div class="box-header with-border">
         <h3 class="box-title">
-          <?= $this->title ?>
+          <?= Html::encode($this->title) ?>
         </h3>
       </div>
       <div class="box-body">
