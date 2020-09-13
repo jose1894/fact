f1de4bbaa8aa8b519a976297d4b9d8dce5326c62
MariaMB17 <marialva1705@gmail.com>
2020-09-13 12:04:26 -0430

    se modifica modulo cliente view / update tittle

--- views/yiisoft/yii2-app/cliente/view.php
+++ views/yiisoft/yii2-app/cliente/view.php
@@ -7,7 +7,7 @@ use kartik\detail\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Cliente */
 
-$this->title = Yii::t('cliente', 'Customer: <span class="label label-primary">{number}</span> {name}', [
+$this->title = Yii::t('cliente', 'Customer: {number} / {name}', [
     'number' => $model->id_clte,
     'name' => $model->nombre_clte
 ]);
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
   <div class="box box-success">
       <div class="box-header with-border">
         <h3 class="box-title">
-          <?= $this->title ?>
+          <?= Html::encode($this->title) ?>
         </h3>
       </div>
       <div class="box-body">
