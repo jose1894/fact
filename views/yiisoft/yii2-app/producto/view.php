2b3e3d25053b8b789565c4fbea76a493200f6725
MariaMB17 <marialva1705@gmail.com>
2020-09-13 14:06:44 -0430

    Se modifica producto view/update tittle

--- views/yiisoft/yii2-app/producto/view.php
+++ views/yiisoft/yii2-app/producto/view.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
   <div class="box box-success">
     <div class="box-header with-border">
       <h3 class="box-title">
-        <?= $this->title ?>
+        <?= Html::encode($this->title) ?>
       </h3>
     </div>
     <div class="box-body">
